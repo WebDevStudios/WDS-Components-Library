6138d9f60c92aba3277b595f784a062eed373475
Carrie Forde <carrie@carrieforde.com>
2017-02-24 10:53:50 -0800

    Add the image hero get function

---
+++ image-hero/image-hero.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Image Hero
+ *
+ * @package _s
+ */
+
+/**
+ * Build the Image Hero markup.
+ *
+ * @param   array  [$args = array()]  The hero arguments.
+ * @return  string                    The hero markup.
+ *
+ * @author Carrie Forde
+ */
+function _s_image_hero( $args = array() ) {
+
+	$defaults = array(
+		'image'         => 'https://unsplash.it/1920/1080',
+		'title'         => 'Image Hero Title',
+		'description'   => 'This is a hero description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mihi enim erit isdem istis fortasse iam utendum. Quare ad ea primum, si videtur; Nec hoc ille non vidit, sed verborum magnificentia est et gloria delectatus.',
+		'button_link'   => '',
+		'button_title'  => '',
+		'button_text'   => '',
+		'class'         => '',
+	);
+	$args = wp_parse_args( $args, $defaults );
+
+	ob_start(); ?>
+
+	<section class="hero-area image-as-background" style="background-image: url( <?php echo esc_url( $args['image'] ); ?> );" role="dialog" aria-labelledby="hero-title" aria-describedby="hero-description">
+		<div class="hero-content">
+			<h2 class="hero-title"><?php echo esc_html( $args['title'] ); ?></h2>
+			<p class="hero-description"><?php echo esc_html( $args['description'] ); ?></p>
+			<a href="<?php echo esc_url( $args['button_link'] ); ?>" class="hero-button" title="<?php echo esc_attr( $args['button_title'] ); ?>"><?php echo esc_html( $args['button_text'] ); ?></a>
+		</div><!-- .hero-content -->
+	</section><!-- .hero-area -->
+	<!-- End Your Code -->
+
+	<?php return ob_get_clean();
+}
+
