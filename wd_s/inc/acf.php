cbdab0bfc8869f7f0f9e14db01465fe4ea57c877
Carrie Forde <carrie@carrieforde.com>
2017-03-13 17:21:24 -0700

    Remove wd_s in favor of making this a Github pages-based resource

--- wd_s/inc/acf.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Custom ACF functions.
- *
- * A place for custom functionality related to Advanced Custom Fields.
- *
- * @package _s
- */
--- wd_s/inc/queries.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Custom queries.
- *
- * This file is used to house "getter" function, that fetch data from the database.
- *
- * @package _s
- */
