42b8e312d76a926786856f5a2fbae5d5da54ccd7
Shannon MacMillan <shannon@webdevstudios.com>
2017-03-30 13:07:44 -0700

    Add variables for tab content areas and links

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,8 +11,18 @@
 	for (var i = 0; i < tabLinks.length; i++) {
 
 		tabLinks[i].addEventListener( 'click', function( e ) {
+
 			// Stop the page from jumping.
 			e.preventDefault;
+
+			// Set up variables for links and content areas.
+			var htmlOutput	= document.getElementById( 'html-output' ),
+				htmlLink	= document.getElementById( 'html-link' ),
+				phpOutput	= document.getElementById( 'php-output' ),
+				phpLink		= document.getElementById( 'php-link' ),
+				scssOutput	= document.getElementById( 'scss-output' ),
+				scssLink	= document.getElementById( 'scss-link' );
+
 		});
     }
 
