9d23e0047f714c0f01776333bc79fb5505775087
Carrie Forde <carrie@carrieforde.com>
2017-05-18 12:25:32 -0700

    Adds a template tags file

---
+++ includes/template-tags.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Custom template tag helpers for the plugin.
+ *
+ * @package  wds-components-library
+ */
+
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Grabbed from wd_s.
+ *
+ * @link  https://github.com/WebDevStudios/wd_s/blob/master/inc/template-tags.php#L14
+ */
+function card_posted_on() {
+
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		esc_html( get_the_modified_date() )
+	);
+
+	$posted_on = sprintf(
+		/* translators: the date the post was published */
+		esc_html_x( 'Posted on %s', 'post date', '_s' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+	$byline = sprintf(
+		/* translators: the post author */
+		esc_html_x( 'by %s', 'post author', '_s' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+
+	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+}
+
+/**
+ * Prints HTML with meta information for the categories, tags and comments.
+ * Grabbed from wd_s.
+ *
+ * @link  https://github.com/WebDevStudios/wd_s/blob/master/inc/template-tags.php#L48
+ */
+function card_entry_footer() {
+
+	// Hide category and tag text for pages.
+	if ( 'post' === get_post_type() ) {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
+		if ( $categories_list && _s_categorized_blog() ) {
+			/* translators: the post category */
+			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+		/* translators: used between list items, there is a space after the comma */
+		$tags_list = get_the_tag_list( '', esc_html__( ', ', '_s' ) );
+		if ( $tags_list ) {
+			/* translators: the post tags */
+			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', '_s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+		}
+	}
+	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( esc_html__( 'Leave a comment', '_s' ), esc_html__( '1 Comment', '_s' ), esc_html__( '% Comments', '_s' ) );
+		echo '</span>';
+	}
+	edit_post_link(
+		sprintf(
+			/* translators: %s: Name of current post */
+			esc_html__( 'Edit %s', '_s' ),
+			the_title( '<span class="screen-reader-text">"', '"</span>', false )
+		),
+		'<span class="edit-link">',
+		'</span>'
+	);
+}
