44593415e7b9661f340d00cf33ddad05a54c5b7f
Carrie Forde <carrie@carrieforde.com>
2017-05-18 09:28:53 -0700

    Adds post thumbnail, and swaps the_content for the_excerpt

--- includes/class-equal-height-cards.php
+++ includes/class-equal-height-cards.php
@@ -122,29 +122,24 @@ class WDSCL_Equal_Height_Cards {
 	public function card_markup() {
 
 		// Start the markup. 🎉 ?>
-		<article <?php post_class(); ?>>
+		<article <?php post_class( 'card' ); ?>>
 			<header class="entry-header">
-				<?php
-				if ( is_single() ) :
-					the_title( '<h1 class="entry-title">', '</h1>' );
-				else :
-					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-				endif;
-				if ( 'post' === get_post_type() ) : ?>
+				<?php if ( has_post_thumbnail() ) : ?>
+					<div class="post-thumbnail">
+						<?php the_post_thumbnail(); ?>
+					</div>
+				<?php endif; ?>
+
+				<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+		
 				<div class="entry-meta">
 					<?php $this->card_posted_on(); ?>
 				</div><!-- .entry-meta -->
-				<?php
-				endif; ?>
 			</header><!-- .entry-header -->
 
 			<div class="entry-content">
 				<?php
-					the_content( sprintf(
-						/* translators: %s: Name of current post. */
-						wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					) );
+					the_excerpt();
 					wp_link_pages( array(
 						'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
 						'after'  => '</div>',
