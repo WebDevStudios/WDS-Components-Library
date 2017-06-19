e7d3eb84315ebc87fd811d90e4f15369d7cefec6
Carrie Forde <carrie@carrieforde.com>
2017-06-19 08:53:49 -0700

    Adds hero image size function

--- template-tags.php
+++ template-tags.php
@@ -4,3 +4,13 @@
  *
  * @package WDS_Components_Library
  */
+
+
+add_filter( 'after_theme_setup', 'wdscl_hero_image_size' );
+/**
+ * Adds Hero Image size.
+ */
+function wdscl_hero_image_size() {
+
+	add_image_size( 'hero-image', 1920, 500, true );
+}
