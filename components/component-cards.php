bcc9e407410f4f498ab7fae0f72d64ac6edd363d
Carrie Forde <carrie@carrieforde.com>
2017-06-19 14:02:26 -0700

    Adds structure for card section

---
+++ components/component-cards.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Cards component.
+ *
+ * This component is used to show sections of cards.
+ *
+ * @since   0.0.0
+ * @package WDS_Components_Library
+ */
+?>
+
+<section class="container cards">
+
+	<div class="row">
+		
+	</div>
+</section>
