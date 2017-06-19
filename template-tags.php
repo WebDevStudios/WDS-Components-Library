cb5f2240e7208b88cd3ef6bd4fafcf4976f38b47
Carrie Forde <carrie@carrieforde.com>
2017-06-19 14:37:14 -0700

    Adds required categorized blog check

--- template-tags.php
+++ template-tags.php
@@ -100,3 +100,30 @@ function card_entry_footer() {
 		'</span>'
 	);
 }
+
+/**
+ * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
+ */
+function _s_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( '_s_categories' ) ) ) {
+		// Create an array of all the categories that are attached to posts.
+		$all_the_cool_cats = get_categories( array(
+			'fields'     => 'ids',
+			'hide_empty' => 1,
+			// We only need to know if there is more than one category.
+			'number'     => 2,
+		) );
+		// Count the number of categories that are attached to the posts.
+		$all_the_cool_cats = count( $all_the_cool_cats );
+		set_transient( '_s_categories', $all_the_cool_cats );
+	}
+	if ( $all_the_cool_cats > 1 ) {
+		// This blog has more than 1 category so _s_categorized_blog should return true.
+		return true;
+	} else {
+		// This blog has only 1 category so _s_categorized_blog should return false.
+		return false;
+	}
+}
