c0ca4a17a656b76bbd06c1df923f2ef4719181c1
Carrie Forde <carrie@carrieforde.com>
2017-06-19 13:57:25 -0700

    Adds generic post card markup

---
+++ components/component-card-post.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * A generic post card.
+ *
+ * @since   0.0.0
+ * @package WDS_Components_Library
+ */
+
+// Start the markup. 🎉 ?>
+<article <?php post_class( 'card' ); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="post-thumbnail">
+				<?php the_post_thumbnail(); ?>
+			</div>
+		<?php endif; ?>
+
+		<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+
+		<div class="entry-meta">
+			<?php card_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_excerpt();
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php card_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
