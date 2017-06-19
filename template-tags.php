d5a9c6b9bb1182aa6a61d6e07eaee925f60591f0
Carrie Forde <carrie@carrieforde.com>
2017-06-19 08:59:09 -0700

    Adds function to register social menu

--- template-tags.php
+++ template-tags.php
@@ -14,3 +14,15 @@ function wdscl_hero_image_size() {
 
 	add_image_size( 'hero-image', 1920, 500, true );
 }
+
+add_action( 'init', 'wdscl_register_social_menu' );
+/**
+ * Register a menu location for the social menu.
+ */
+function wdscl_register_social_menu() {
+
+	// Register a social nav menu location.
+	register_nav_menus( array(
+		'social' => esc_html__( 'Social Menu', 'wds-component-library' ),
+	) );
+}
